2879c5235d05b8f36e35266ad3c9afed592d4bd4
Bilgin Torun Uzman CRM <bilgin.torun@uzmancrm.com>
2026-04-13 11:44:17 +0300

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Yerel geliştirme için en temiz ayar
+  base: '/HaberUygulamasi/', // GitHub Pages repo adı
 })
